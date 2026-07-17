3d1f3d7f0aa36c7c82b84395059dc367768cd590
viplavg <viplavg1999@gmail.com>
2026-07-17 23:34:18 +0530

    user model created

---
+++ server/src/modules/users/user.model.js
@@ -0,0 +1,61 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    enum: ["CUSTOMER", "AGENT", "ADMIN"],
+    default: "CUSTOMER",
+  },
+},{timestamps: true});
+
+userSchema.pre("save", async function() {
+    try {
+        if(!this.isModified("password")) {
+        return;
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+    } catch (error) {
+        throw error;
+    }
+})
+
+userSchema.methods.comparePassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateToken = function() {
+    return jwt.sign(
+        {
+            id: this._id,
+            role: this.role,
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: process.env.JWT_EXPIRES_IN,
+
+        }
+    );
+}
+
+const User = mongoose.model("User", userSchema);
+
+export default User;

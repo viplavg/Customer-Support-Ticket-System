3ee36d72b8a44a87258a8be7da4536d89a04c3b0
viplavg <viplavg1999@gmail.com>
2026-07-20 20:30:32 +0530

    Admin dashboard aggregation pipeline also implemented

--- server/src/modules/dashboard/dashboard.service.js
+++ server/src/modules/dashboard/dashboard.service.js
@@ -148,4 +148,69 @@ const getAgentDashboard = async ({ userId }) => {
   );
 };
 
-const getAdminDashboard = async ({ userId }) => {};
+const getAdminDashboard = async () => {
+  const dashboard = await Ticket.aggregate([
+    {
+      $group: {
+        _id: null,
+        totalTickets: { $sum: 1 },
+
+        openTickets: {
+          $sum: {
+            $cond: [{ $eq: ["$status", "OPEN"] }, 1, 0],
+          },
+        },
+
+        inProgressTickets: {
+          $sum: {
+            $cond: [{ $eq: ["$status", "IN_PROGRESS"] }, 1, 0],
+          },
+        },
+
+        waitingForCustomerTickets: {
+          $sum: {
+            $cond: [
+              { $eq: ["$status", "WAITING_FOR_CUSTOMER"] },
+              1,
+              0,
+            ],
+          },
+        },
+
+        resolvedTickets: {
+          $sum: {
+            $cond: [{ $eq: ["$status", "RESOLVED"] }, 1, 0],
+          },
+        },
+
+        closedTickets: {
+          $sum: {
+            $cond: [{ $eq: ["$status", "CLOSED"] }, 1, 0],
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        totalTickets: 1,
+        openTickets: 1,
+        inProgressTickets: 1,
+        waitingForCustomerTickets: 1,
+        resolvedTickets: 1,
+        closedTickets: 1,
+      },
+    },
+  ]);
+
+  return (
+    dashboard[0] || {
+      totalTickets: 0,
+      openTickets: 0,
+      inProgressTickets: 0,
+      waitingForCustomerTickets: 0,
+      resolvedTickets: 0,
+      closedTickets: 0,
+    }
+  );
+};
